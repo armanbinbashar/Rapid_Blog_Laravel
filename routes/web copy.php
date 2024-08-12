3172f7f65b0d19ff1fb0910b3d2ac2251af3cbcd
RmanBD <arman0987@gmail.com>
2024-08-12 18:31:46 +0600

    env setup

---
+++ routes/web copy.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Route;
+
+
+// using closure
+// Route::get('/', function () {
+//     // fetch posts from db
+//     // fetch category from db
+//     return view('welcome');
+// });
+
+
+// using controller
+// to welcome page
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome.index');
+
+
+// to blog page
+Route::get('/blog', [BlogController::class, 'index'])->name('blog.show');
+
+
+// to single blog post
+Route::get('/blog/post', [BlogController::class, 'show'])->name('blog.show');
+
+
+// to about page
+Route::get('/rapidpost-about-us', function(){
+    return view('about');
+})->name('about');
+
+
+// to contact page
+Route::get('/rapidpost-contact-us', [ContactController::class, 'index'])->name('contact.index');
+
+
