2f7d79b64523b832fb379c3a7cc4696075c9a0f5
RmanBD <arman0987@gmail.com>
2024-08-12 20:11:42 +0600

    install a login reg dependenices

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,20 @@
 <?php
 
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// using closure
-// Route::get('/', function () {
-//     // fetch posts from db
-//     // fetch category from db
-//     return view('welcome');
-// });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// using controller
-// to welcome page
-Route::get('/', [WelcomeController::class, 'index'])->name('welcome.index');
-
-
-// to blog page
-Route::get('/blog', [BlogController::class, 'index'])->name('blog.show');
-
-
-// to single blog post
-Route::get('/blog/post', [BlogController::class, 'show'])->name('blog.show');
-
-
-// to about page
-Route::get('/rapidpost-about-us', function(){
-    return view('about');
-})->name('about');
-
-
-// to contact page
-Route::get('/rapidpost-contact-us', [ContactController::class, 'index'])->name('contact.index');
-
-
+require __DIR__.'/auth.php';
