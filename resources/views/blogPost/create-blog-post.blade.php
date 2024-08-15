eb5e0a4105554456ba5196fb5a51cff07e035015
RmanBD <arman0987@gmail.com>
2024-08-15 21:18:59 +0600

    add bootstrap framework

--- resources/views/blogPost/create-blog-post.blade.php
+++ resources/views/blogPost/create-blog-post.blade.php
@@ -1,7 +1,8 @@
 @extends('layout')
 
 @section('head')
-    {{-- <link rel="stylesheet" href="render only on create-blog-post page"> --}}
+    <link rel="stylesheet" href="./res/css/bootstrap.min.css">
+    <script defer src="./res/js/bootstrap.bundle.min.js"></script>
 @endsection
 
 
@@ -9,8 +10,23 @@
     <main>
         <section id="contact-us">
             <h1 style="padding-top: 50px">Create New Post!</h1>
-            
-            <div class="contact-form">
+
+            <div class="container">
+                <form action="" novalidate>
+                    <div class="form-floating">
+                        <input type="email" id="email" class="form-control" placeholder="email place here" required>
+                        <label for="email" class="form-label">Email</label>
+                        <div class="invalid-feedback">Invalid Email</div>
+                        <div class="valid-feedback">Valid Email</div>
+                    </div>
+                    <br>
+        
+                    <button class="btn btn-primary">Submit</button>
+        
+                </form>
+            </div>
+
+            {{-- <div class="contact-form">
 
                 <form action="{{route('blog.store')}}" method="post" enctype="multipart/form-data">
                     @csrf
@@ -35,8 +51,21 @@
                     
                     <button type="submit" value="submit">Submit</button>
                 </form>
-            </div>
+            </div> --}}
         </section>
     </main>
 @endsection
+
+@section('scripts')
+<script>
+    const form = document.querySelector('form')
+    form.addEventListener('submit', e => {
+        if (!form.checkValidity()) {
+            e.preventDefault()
+        }
+        form.classList.add('was-validated')
+    })
+</script>
+@endsection
+
 
