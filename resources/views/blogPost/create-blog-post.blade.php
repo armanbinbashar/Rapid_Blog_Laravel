68d4e321fb91dd91c1eff46a95d2b61b07b1f1b8
RmanBD <arman0987@gmail.com>
2024-08-15 22:30:47 +0600

    customize create post form

--- resources/views/blogPost/create-blog-post.blade.php
+++ resources/views/blogPost/create-blog-post.blade.php
@@ -1,36 +1,21 @@
 @extends('layout')
 
 @section('head')
-    <link rel="stylesheet" href="./res/css/bootstrap.min.css">
-    <script defer src="./res/js/bootstrap.bundle.min.js"></script>
+    {{-- <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script> --}}
 @endsection
 
 
 @section('main')
-    <main>
+    <div style="padding: 5%">
         <section id="contact-us">
+
             <h1 style="padding-top: 50px">Create New Post!</h1>
 
-            <div class="container">
-                <form action="" novalidate>
-                    <div class="form-floating">
-                        <input type="email" id="email" class="form-control" placeholder="email place here" required>
-                        <label for="email" class="form-label">Email</label>
-                        <div class="invalid-feedback">Invalid Email</div>
-                        <div class="valid-feedback">Valid Email</div>
-                    </div>
-                    <br>
-        
-                    <button class="btn btn-primary">Submit</button>
-        
-                </form>
-            </div>
-
-            {{-- <div class="contact-form">
-
+            <div class="contact-form">
                 <form action="{{route('blog.store')}}" method="post" enctype="multipart/form-data">
                     @csrf
-                    <label for="title"></label>
+                    <label for="title">Title</label>
                     <input type="text" id="title" name="title">
 
                     @error('title')
@@ -51,21 +36,47 @@
                     
                     <button type="submit" value="submit">Submit</button>
                 </form>
-            </div> --}}
+            </div>
         </section>
-    </main>
+    </div>
+
+
+    {{-- <div class="container">
+
+        <h1 style="margin-top: 10%; text-align: center">Create New Post!</h1>
+
+        <form action="">
+            <div class="form-floating" style="margin-top: 30px">
+                <input type="Title" id="title" class="form-control" placeholder="Title place here" required>
+                <label for="title" class="form-label">Title</label>
+                <div class="invalid-feedback">Invalid Email</div>
+                <div class="valid-feedback">Valid Email</div>
+            </div>
+            <div class="form" style="margin-top: 15px">
+                <input type="file" name="image" id="postimage" class="form-control" placeholder="select a image file." required>
+                @error('image')
+                    <p style="color: red; margin-bottom:20px">{{$message}}</p>
+                @enderror
+            </div>
+            <br>
+
+            <button class="btn btn-primary">Submit</button>
+
+        </form>
+    </div> --}}
+
 @endsection
 
 @section('scripts')
-<script>
-    const form = document.querySelector('form')
-    form.addEventListener('submit', e => {
-        if (!form.checkValidity()) {
-            e.preventDefault()
-        }
-        form.classList.add('was-validated')
-    })
-</script>
+    <script>
+        const form = document.querySelector('form')
+        form.addEventListener('submit', e => {
+            if (!form.checkValidity()) {
+                e.preventDefault()
+            }
+            form.classList.add('was-validated')
+        })
+    </script>
 @endsection
 
 
